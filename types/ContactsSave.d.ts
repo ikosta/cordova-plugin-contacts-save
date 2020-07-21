353e329b3a6228290ab6181a7cc580a262d734c8
Konstantinos Tsanakas <konstantinos.tsanakas@gmail.com>
2020-07-21 19:05:17 +0200

    fix(): types

--- types/ContactsSave.d.ts
+++ types/ContactsSave.d.ts
@@ -1,21 +1,19 @@
-interface CNMutableContact {
-  namePrefix?: string;
-  givenName?: string;
-  familyName?: string;
-}
+declare module "cordova-plugin-contacts-save" {
+  type ContactsSaveSuccessHandler = (response: string) => void;
+  type ContactsSaveErrorHandler = (error: string) => void;
 
-type ContactsSaveSuccessHandler = (response: string) => void;
-type ContactsSaveErrorHandler = (error: string) => void;
+  interface CNMutableContact {
+    namePrefix?: string;
+    givenName?: string;
+    familyName?: string;
+  }
 
-declare class ContactsSave {
-  saveContacts(
-    contacts?: CNMutableContact[],
-    container?: string,
-    onSuccess?: ContactsSaveSuccessHandler,
-    onError?: ContactsSaveErrorHandler
-  ): void;
-}
-
-interface Window {
-  ContactsSave: ContactsSave;
+  class ContactsSave {
+    static saveContacts: (
+      contacts?: CNMutableContact[],
+      container?: string,
+      onSuccess?: ContactsSaveSuccessHandler,
+      onError?: ContactsSaveErrorHandler
+    ) => void;
+  }
 }
