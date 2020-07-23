371ea751e0d002646430a301c9bb28002cc1045d
Konstantinos Tsanakas <konstantinos.tsanakas@gmail.com>
2020-07-23 11:28:20 +0200

    add(): save contacts

--- README.md
+++ README.md
@@ -1,3 +1,87 @@
 # cordova-plugin-contacts-save
 
-Cordova Contacts Save Plugin
+[![npm version](https://img.shields.io/npm/v/cordova-plugin-contacts-save)](https://www.npmjs.com/package/cordova-plugin-contacts-save?activeTab=versions)
+[![MIT Licence](https://img.shields.io/badge/license-MIT-blue?style=flat)](https://opensource.org/licenses/mit-license.php)
+
+This plugin saves the contacts into the device.
+
+- [Installation](#installation)
+- [Supported Platforms](#supported-platforms)
+- [Usage](#usage)
+- [Types](#types)
+
+## Installation
+
+```shell
+cordova plugin add cordova-plugin-contacts-save
+```
+
+## Supported Platforms
+
+- iOS
+
+## Usage
+
+```js
+// define contacts
+var contacts = [
+  {
+    givenName: "Steve",
+    familyName: "Jobs",
+  },
+];
+
+// set default store container
+var container = "";
+
+// save contacts
+window.ContactsSave.saveContacts(
+  contacts,
+  container,
+  (cookies) => {
+    // contacts saved
+  },
+  (error) => {
+    // error: NO_CONTACTS or NO_PERMISSON
+  }
+);
+```
+
+## Types
+
+```ts
+export interface CNMutableContact {
+  namePrefix?: string;
+  givenName?: string;
+  familyName?: string;
+  organizationName?: string;
+  jobTitle?: string;
+  postalAddresses: CNLabeledValue<"home" | "work" | "other", CNPostalAddress>[];
+  phoneNumbers: CNLabeledValue<
+    "home" | "work" | "mobile" | "homeFax" | "workFax" | "otherFax" | "other",
+    CNPhoneNumber
+  >[];
+  emailAddresses: CNLabeledValue<"home" | "work" | "other", string>[];
+  urlAddresses: CNLabeledValue<"home" | "work" | "other", string>[];
+  imageData?: string;
+  note?: string;
+}
+
+export interface CNLabeledValue<L, V> {
+  label?: L;
+  value: V;
+}
+
+export interface CNPhoneNumber {
+  stringValue?: string;
+}
+
+export interface CNPostalAddress {
+  street?: string;
+  city?: string;
+  state?: string;
+  postalCode?: string;
+  ISOCountryCode?: string;
+  country?: string;
+}
+```
--- src/ios/ContactsSave.m
+++ src/ios/ContactsSave.m
@@ -22,23 +22,186 @@
 
     // check contacts argument
     if ([contacts isEqual:[NSNull null]] || contacts.count == 0) {
-        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Contacts missing."] callbackId:command.callbackId];
+        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NO_CONTACTS"] callbackId:command.callbackId];
         return;
     }
-    
+
     // check container argument
     if ([container isEqual:[NSNull null]] || container.length == 0) {
         container = nil;
     }
-    
+
     // save contacts
-    [contacts enumerateObjectsUsingBlock:^(id _Nonnull obj, NSUInteger idx, BOOL* _Nonnull stop) {
+    [contacts enumerateObjectsUsingBlock:^(NSMutableDictionary* _Nonnull contact, NSUInteger idContacts, BOOL* _Nonnull stopContacts) {
         // create mutable contact
         CNMutableContact* mutableContact = [[CNMutableContact alloc] init];
 
-        // populate mutable contact
-        mutableContact.givenName = @"Kosta";
-        mutableContact.familyName = @"Tsanakas";
+        /**
+         Name
+         */
+        mutableContact.namePrefix = [contact[@"namePrefix"] stringValue];
+        mutableContact.givenName = [contact[@"givenName"] stringValue];
+        mutableContact.familyName = [contact[@"familyName"] stringValue];
+
+        /**
+         Organization
+         */
+        mutableContact.organizationName = [contact[@"organizationName"] stringValue];
+        mutableContact.jobTitle = [contact[@"jobTitle"] stringValue];
+
+        /**
+         Phone Numbers
+         */
+        NSMutableArray* mutablePhoneNumbers =  [[NSMutableArray alloc] init];
+
+        // iterate phone numbers
+        NSArray* phoneNumbers = contact[@"phoneNumbers"];
+        [phoneNumbers enumerateObjectsUsingBlock:^(NSMutableDictionary* _Nonnull labeledItem, NSUInteger idAddresses, BOOL* _Nonnull stopAddresses) {
+            // get phone number label
+            NSString* label = [labeledItem[@"label"] stringValue];
+
+            // get phone number value
+            NSDictionary* value = labeledItem[@"value"];
+
+            // check phone number value
+            NSString* stringValue = [value[@"stringValue"] stringValue];
+            if (stringValue != nil) {
+                // create phone number
+                CNPhoneNumber* phoneNumber =[CNPhoneNumber phoneNumberWithStringValue:stringValue];
+
+                // add to mutable phone numbers
+                if ([label isEqualToString:@"home"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelHome value:phoneNumber]];
+                } else if ([label isEqualToString:@"work"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelWork value:phoneNumber]];
+                } else if ([label isEqualToString:@"mobile"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelPhoneNumberMobile value:phoneNumber]];
+                } else if ([label isEqualToString:@"homeFax"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelPhoneNumberHomeFax value:phoneNumber]];
+                } else if ([label isEqualToString:@"workFax"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelPhoneNumberWorkFax value:phoneNumber]];
+                } else if ([label isEqualToString:@"otherFax"]) {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelPhoneNumberOtherFax value:phoneNumber]];
+                } else {
+                    [mutablePhoneNumbers addObject:[CNLabeledValue labeledValueWithLabel:CNLabelOther value:phoneNumber]];
+                }
+            }
+        }];
+
+        // populate mutable phone numbers
+        mutableContact.phoneNumbers = mutablePhoneNumbers;
+
+        /**
+         Email Addresses
+         */
+        NSMutableArray* mutableEmailAddresses =  [[NSMutableArray alloc] init];
+
+        // iterate email addresses
+        NSArray* emailAddresses = contact[@"emailAddresses"];
+        [emailAddresses enumerateObjectsUsingBlock:^(NSMutableDictionary* _Nonnull labeledItem, NSUInteger idAddresses, BOOL* _Nonnull stopAddresses) {
+            // get email address label
+            NSString* label = [labeledItem[@"label"] stringValue];
+
+            // get email address value
+            NSString* value = [labeledItem[@"value"] stringValue];
+
+            // check email address value
+            if (value != nil) {
+                // add to mutable email addresses
+                if ([label isEqualToString:@"home"]) {
+                    [mutableEmailAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelHome value:value]];
+                } else if ([label isEqualToString:@"work"]) {
+                    [mutableEmailAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelWork value:value]];
+                } else {
+                    [mutableEmailAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelOther value:value]];
+                }
+            }
+        }];
+
+        // populate mutable email addresses
+        mutableContact.emailAddresses = mutableEmailAddresses;
+
+        /**
+         Url Addresses
+         */
+        NSMutableArray* mutableUrlAddresses =  [[NSMutableArray alloc] init];
+
+        // iterate email addresses
+        NSArray* urlAddresses = contact[@"urlAddresses"];
+        [urlAddresses enumerateObjectsUsingBlock:^(NSMutableDictionary* _Nonnull labeledItem, NSUInteger idAddresses, BOOL* _Nonnull stopAddresses) {
+            // get email address label
+            NSString* label = [labeledItem[@"label"] stringValue];
+
+            // get email address value
+            NSString* value = [labeledItem[@"value"] stringValue];
+
+            // check email address value
+            if (value != nil) {
+                // add to mutable email addresses
+                if ([label isEqualToString:@"home"]) {
+                    [mutableUrlAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelHome value:value]];
+                } else if ([label isEqualToString:@"work"]) {
+                    [mutableUrlAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelWork value:value]];
+                } else {
+                    [mutableUrlAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelOther value:value]];
+                }
+            }
+        }];
+
+        // populate mutable email addresses
+        mutableContact.urlAddresses = mutableUrlAddresses;
+
+        /**
+         Postal Addresses
+         */
+        NSMutableArray* mutablePostalAddresses =  [[NSMutableArray alloc] init];
+
+        // iterate postal addresses
+        NSArray* postalAddresses = contact[@"postalAddresses"];
+        [postalAddresses enumerateObjectsUsingBlock:^(NSMutableDictionary* _Nonnull labeledItem, NSUInteger idAddresses, BOOL* _Nonnull stopAddresses) {
+            // get postal address label
+            NSString* label = [labeledItem[@"label"] stringValue];
+
+            // get postal address value
+            NSDictionary* value = labeledItem[@"value"];
+
+            // create mutable postal address
+            CNMutablePostalAddress* mutablePostalAddress = [[CNMutablePostalAddress alloc] init];
+
+            // populate mutable postal address
+            mutablePostalAddress.street = [value[@"street"] stringValue];
+            mutablePostalAddress.city = [value[@"city"] stringValue];
+            mutablePostalAddress.state = [value[@"state"] stringValue];
+            mutablePostalAddress.postalCode = [value[@"postalCode"] stringValue];
+            mutablePostalAddress.ISOCountryCode = [value[@"ISOCountryCode"] stringValue];
+            if (mutablePostalAddress.ISOCountryCode == nil || mutablePostalAddress.ISOCountryCode.length == 0) {
+                mutablePostalAddress.country = [value[@"country"] stringValue];
+            }
+
+            // add to mutable postal addresses
+            if ([label isEqualToString:@"home"]) {
+                [mutablePostalAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelHome value:mutablePostalAddress]];
+            } else if ([label isEqualToString:@"work"]) {
+                [mutablePostalAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelWork value:mutablePostalAddress]];
+            } else {
+                [mutablePostalAddresses addObject:[CNLabeledValue labeledValueWithLabel:CNLabelOther value:mutablePostalAddress]];
+            }
+        }];
+
+        // populate mutable postal addresses
+        mutableContact.postalAddresses = mutablePostalAddresses;
+
+        /**
+         Image Data
+         */
+        if (contact[@"imageData"] != nil) {
+            mutableContact.imageData = [[NSData alloc] initWithBase64EncodedString:contact[@"imageData"] options:0];
+        }
+
+        /**
+         Note
+         */
+        mutableContact.note = contact[@"note"];
 
         // create save request
         CNSaveRequest* saveRequest = [[CNSaveRequest alloc] init];
@@ -50,15 +213,15 @@
         CNContactStore* store = [[CNContactStore alloc] init];
 
         // execute save request
-        if(![store executeSaveRequest:saveRequest error:nil]) {
+        if (![store executeSaveRequest:saveRequest error:nil]) {
             // save request failed because of missing permission
-            [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Permission missing."] callbackId:command.callbackId];
+            [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NO_PERMISSON"] callbackId:command.callbackId];
             return;
         }
     }];
-    
+
     // return success
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Contacts saved."] callbackId:command.callbackId];
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
 }
 
 @end
